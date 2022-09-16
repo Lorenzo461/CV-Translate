94953b92c3b8b0c5fab249fd748de7af98845295
Lorenzo461 <romeolorenzo@gmail.com>
2022-09-16 17:50:31 +0200

    add header

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import '../css/Header.css';
+import strada from '../img/strada.png';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='container-header'>
+         <h1> CV Translate</h1>
+         <h2>Tutte le strade portano al tuo nuovo lavoro!</h2>
+        <div >
+        <img src={strada} alt="tranlsate" />
+        </div>
+    </div>
   )
 }
